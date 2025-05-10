59683d34dbc2fff0f47ddce28855f255dcb68de7
Lintaaa-25 <lintababu3@gmail.com>
2025-05-10 23:49:26 +0530

    Update useLocalStorage.ts

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,13 +1,14 @@
-// useLocalStorage.ts
 import { useState } from 'react';
 
 function useLocalStorage<T>(key: string, initialValue: T) {
   const [storedValue, setStoredValue] = useState<T>(() => {
     try {
       const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : initialValue;
+      // Ensure that the parsed value is an array, or use the initial value
+      const parsedValue = item ? JSON.parse(item) : initialValue;
+      return Array.isArray(parsedValue) ? parsedValue : initialValue;
     } catch (error) {
-      console.error(error);
+      console.error('Error reading from localStorage', error);
       return initialValue;
     }
   });
@@ -17,7 +18,7 @@ function useLocalStorage<T>(key: string, initialValue: T) {
       setStoredValue(value);
       window.localStorage.setItem(key, JSON.stringify(value));
     } catch (error) {
-      console.error(error);
+      console.error('Error writing to localStorage', error);
     }
   };
 
