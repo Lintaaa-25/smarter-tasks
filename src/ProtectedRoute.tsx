22f89de54b3257cf7f03e26c414e36e3771116c8
Lintaaa-25 <lintababu3@gmail.com>
2025-05-27 23:49:03 +0530

    Update ProtectedRoute.tsx

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -1,10 +1,8 @@
 import { Navigate } from "react-router-dom";
 
-export default function ProtectedRoute({ children }: { children: JSX.Element }) {
+const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   const authenticated = !!localStorage.getItem("authToken");
-  if (authenticated) {
-    return <>{children}</>;
-  } else {
-    return <Navigate to="/signin" />;
- }
-}
+  return authenticated ? children : <Navigate to="/signin" />;
+};
+
+export default ProtectedRoute;
