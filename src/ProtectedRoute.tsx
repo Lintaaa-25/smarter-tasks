295257d794727dccbf91ea2d917eb821a31f9973
Lintaaa-25 <lintababu3@gmail.com>
2025-05-29 12:10:18 +0530

    Delete src/ProtectedRoute.tsx

--- src/ProtectedRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-  const authenticated = !!localStorage.getItem("authToken");
-  return authenticated ? children : <Navigate to="/signin" />;
-};
-
-export default ProtectedRoute;
