1b12b9a58f27015dd31b2f2d265995b725325969
Lintaaa-25 <lintababu3@gmail.com>
2025-05-29 12:10:44 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,22 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Notfound from "./pages/Notfound";
-import Signup from "./pages/signup";
-import Signin from "./pages/signin";
-import Dashboard from "./pages/dashboard";
-import ProtectedRoute from "./ProtectedRoute";
+import React, { useContext } from "react";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
+import { ThemeContext } from "./context/theme";
+import { ProjectsProvider } from "./context/projects/context";
+import { MembersProvider } from "./context/members/context";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Signup />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/signin",
-    element: <Signin />,
-  },
-  {
-    path: "/dashboard",
-    element: (
-      <ProtectedRoute>
-        <Dashboard />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: "/notfound",
-    element: <Notfound />,
-  },
-  {
-    path: "*",
-    element: <Notfound />,
-  },
-]);
+const App = () => {
+  const { theme } = useContext(ThemeContext);
 
-const App: React.FC = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <div className={`h-screen w-full mx-auto py-2 ${theme === "dark" ? "dark" : ""}`}>
+      <ProjectsProvider>
+        <MembersProvider>
+          <RouterProvider router={router} />
+        </MembersProvider>
+      </ProjectsProvider>
+    </div>
+  );
 };
 
 export default App;
